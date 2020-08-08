00222c492b0c1e9f2f83dcccf24e54c57c706117
ZelliDev <zellifone@gmail.com>
2020-08-08 11:10:07 -0300

    Upload The Test Programs

---
+++ CSharpTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Quantum.Simulation.Simulators;
+using Quantum.QSharpRandom;
+
+
+namespace Test
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var sim = new QuantumSimulator();
+            var QsharpResponse = await GenerateNumber.Run(sim,50);
+            Console.WriteLine(QsharpResponse.ToString());
+            Console.ReadLine();
+            
+        }
+    }
+}
